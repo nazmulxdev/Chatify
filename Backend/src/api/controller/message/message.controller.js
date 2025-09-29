0900df4a4f00cc9c3c2dd7c043542a434833d350
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-09-29 18:55:47 +0600

    added all masseging route

--- Backend/src/api/controller/message/message.controller.js
+++ Backend/src/api/controller/message/message.controller.js
@@ -1,9 +1,104 @@
-const message = (req, res) => {
+import cloudinary from "../../../config/cloudinary.config.js";
+import Message from "../../../models/Message.js";
+import User from "../../../models/User.js";
+
+const getAllContacts = async (req, res) => {
   try {
-    res.send("this is message route");
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
   } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Errors in getAllContacts controller." });
+  }
+};
+const getMessageById = async (req, res) => {
+  try {
+    const myId = req.user._id;
+    const { id } = req.params;
+    const message = await Message.find({
+      $or: [
+        {
+          senderId: myId,
+          receiverId: id,
+        },
+        {
+          senderId: id,
+          receiverId: myId,
+        },
+      ],
+    });
+    res.status(200).json(message);
+  } catch (error) {
+    res.status(500).send({ message: "Error in the message controller." });
+  }
+};
+
+const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+    let imageUrl;
+    if (image) {
+      // upload base64 image to cloudinary
+      const uploadImage = await cloudinary.uploader(image);
+      imageUrl = uploadImage.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+    // todo: send message in real time if user is online.
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.log(error);
     res.status(500).send({ message: "Server Error" });
   }
 };
 
-export { message };
+const getAllChats = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    // find all the messages where the logged in user is either sender or receiver
+
+    const messages = await Message.find({
+      $or: [
+        {
+          senderId: loggedInUserId,
+        },
+        {
+          receiverId: loggedInUserId,
+        },
+      ],
+    });
+
+    const chatPartnersIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUserId
+            ? msg.receiverId.toString()
+            : msg.senderId.toString(),
+        ),
+      ),
+    ];
+
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnersIds },
+    }).select("-password");
+
+    res.status(200).json(chatPartners);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error from message controller." });
+  }
+};
+
+export { getMessageById, sendMessage, getAllChats, getAllContacts };
--- Backend/src/api/router/message/message.route.js
+++ Backend/src/api/router/message/message.route.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
-import { message } from "../../controller/message/message.controller.js";
+import {
+  getAllChats,
+  getAllContacts,
+  getMessageById,
+  sendMessage,
+} from "../../controller/message/message.controller.js";
+import { verifyToken } from "../../middlewares/auth.middleware.js";
+import arcjetProtection from "../../middlewares/arcjet.middleware.js";
 
 const router = Router();
-router.get("/message", message);
+router.use(arcjetProtection, verifyToken);
+router.get("/contacts", getAllContacts);
+router.get("/chats", getAllChats);
+router.get("/:id/messages", getMessageById);
+router.post("/send/:id", sendMessage);
 
 export default router;
